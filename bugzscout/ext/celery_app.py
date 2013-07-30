1abda83128577616f94d5d4a0cd0393eaf22671a
Thomas Van Doren <thomas.vandoren@gmail.com>
2013-07-30 15:39:38 -0700

    Add docstring to celery extension.

--- bugzscout/ext/celery_app.py
+++ bugzscout/ext/celery_app.py
@@ -29,6 +29,17 @@ celery = Celery()
 @celery.task
 def submit_error(url, user, project, area, description,
                  extra=None, default_message=None):
+    """Celery task for submitting errors asynchronously.
+
+    :param url: string URL for bugzscout
+    :param user: string fogbugz user to designate when submitting
+                 via bugzscout
+    :param project: string fogbugz project to designate for cases
+    :param area: string fogbugz area to designate for cases
+    :param description: string description for error
+    :param extra: string details for error
+    :param default_message: string default message to return in responses
+    """
     LOG.debug('Creating new BugzScout instance.')
     client = bugzscout.BugzScout(
         url, user, project, area)
